5b15700468211c56d0812ae9f89996c38259b151
zinian <361154416@qq.com>
2021-04-18 02:45:23 +0800

    Update ss-rust.sh

--- ss-rust.sh
+++ ss-rust.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 read -p "Enter SS_VER: " SS_VER
 read -p "Enter V2rayP_VER: " V2rayP_VER
 wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/$SS_VER/shadowsocks-$SS_VER.x86_64-unknown-linux-gnu.tar.xz && tar -xvJf  shadowsocks-$SS_VER.x86_64-unknown-linux-gnu.tar.xz  -C /usr/local/bin && rm -rf shadowsocks-$SS_VER.x86_64-unknown-linux-gnu.tar.xz
