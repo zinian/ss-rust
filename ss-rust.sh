7efd8ff886ca9fdd0b1b2795d4090559ef8dfce6
zinian <361154416@qq.com>
2021-04-18 15:57:22 +0800

    Update ss-rust.sh

--- ss-rust.sh
+++ ss-rust.sh
@@ -7,12 +7,17 @@ restart_rc="0"
 
 if [ $SS_VER != $ss_ver ];then
   echo shadowsocks-rust begin update $ss_ver to $SS_VER ...
-  wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/$SS_VER/shadowsocks-$SS_VER.x86_64-unknown-linux-gnu.tar.xz && tar -xvJf  shadowsocks-$SS_VER.x86_64-unknown-linux-gnu.tar.xz  -C /usr/local/bin && rm -rf shadowsocks-$SS_VER.x86_64-unknown-linux-gnu.tar.xz
+  wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/$SS_VER/shadowsocks-$SS_VER.x86_64-unknown-linux-gnu.tar.xz &&\
+  tar -xvJf  shadowsocks-$SS_VER.x86_64-unknown-linux-gnu.tar.xz  -C /usr/local/bin &&\
+  rm -rf shadowsocks-$SS_VER.x86_64-unknown-linux-gnu.tar.xz  &&\
   restart_rc="1"
 fi
 if [ $V2_VER != $v2_ver ];then
   echo v2ray-plugin begin update $v2_ver to $V2_VER ...
-  wget https://github.com/shadowsocks/v2ray-plugin/releases/download/$V2_VER/v2ray-plugin-linux-amd64-$V2_VER.tar.gz && tar -xvf  v2ray-plugin-linux-amd64-$V2_VER.tar.gz -C /usr/local/bin && rm -rf v2ray-plugin-linux-amd64-$V2_VER.tar.gz && mv /usr/local/bin/v2ray-plugin_linux_amd64 /usr/local/bin/v2ray-plugin
+  wget https://github.com/shadowsocks/v2ray-plugin/releases/download/$V2_VER/v2ray-plugin-linux-amd64-$V2_VER.tar.gz &&\
+  tar -xvf  v2ray-plugin-linux-amd64-$V2_VER.tar.gz -C /usr/local/bin &&\
+  rm -rf v2ray-plugin-linux-amd64-$V2_VER.tar.gz &&\
+  mv -f /usr/local/bin/v2ray-plugin_linux_amd64 /usr/local/bin/v2ray-plugin  &&\
   restart_rc="1"
 fi
 if [ $restart_rc == "1" ];then
